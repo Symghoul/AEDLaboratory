2757e6160650d29f1d708e368c342753faa0d113
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2020-02-15 16:43:52 -0500

    Preparing Junit

---
+++ Laboratory1/srcTest/testModel/PrimeTest.java
@@ -0,0 +1,24 @@
+package testModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Prime;
+
+class PrimeTest {
+	
+	private Prime prime;
+
+	
+	void setUp1() {
+		prime = new Prime();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
