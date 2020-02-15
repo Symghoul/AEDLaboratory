e0fd14df40958ad98196b98c2e89943f078a2000
alejandraEstacio <alejandraestacio@gmail.com>
2020-02-15 18:23:34 -0500

    eratostenes method

--- Laboratory1/src/model/Prime.java
+++ Laboratory1/src/model/Prime.java
@@ -59,9 +59,24 @@ public class Prime {
 	/**
 	 * Verify the primes in Numbers using the eratostenes algorithm
 	 */
-	public void verifyPrimeEratostenes() {
+	public void verifyPrimeEratostenes(int n) {
 		
+		 boolean prime[] = new boolean[n+1];
+	        
+	        for(int i=0;i<n;i++)
+	        	prime[i] = true;
+	        
+	        for(int p = 2; p*p <=n; p++) {
+	            if(prime[p] == true){
+	                for(int i = p*2; i <= n; i += p)
+	                	for(int j = p*2; j <= n-1; j += p)
+	                    prime[i] = false;
+	            }
+	        }    	
 	}
+
+		
+	
 	
 	/**
 	 * Verify the primes in Numbers using the Euler factorization
